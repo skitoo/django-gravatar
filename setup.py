4c31402ab6c465dc731ea2f8a3086ce433e9e5c2
skitoo <alexis.couronne@gmail.com>
2011-12-15 13:12:59 +0100

    add AUTHORS file add LICENSE file add README file add setup.py file

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import django_gravatar, os
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+      author = 'Alexis Couronne',
+      author_email = 'alexis.couronne@scopart.fr',
+      name = 'django-gravatar',
+      version = django_gravatar.__version__,
+      description = 'Django Gravatar is a lightweight Django application that allows you to insert a Gravatar image in your templates.',
+      long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+      keywords = 'django gravatar avatar',
+      url = 'https://github.com/skitoo/django-gravatar',
+      license = 'New BSD License',
+      platforms = ['OS Independent'],
+      classifiers = CLASSIFIERS,
+      install_requires = ['Django>=1.3.1'],
+      packages = find_packages() 
+)
