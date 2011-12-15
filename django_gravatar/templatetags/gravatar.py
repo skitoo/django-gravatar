cdaa0ab18febbc9b3f26a4c813d7e3bb0ef8bb15
skitoo <alexis.couronne@gmail.com>
2011-12-15 09:50:28 +0100

    rename "gravatar" tag to "gravatar_url" Adding a tag "gravatar" which returns an html tag img

--- django_gravatar/templatetags/gravatar.py
+++ django_gravatar/templatetags/gravatar.py
@@ -5,6 +5,14 @@ import urllib, hashlib
 
 register = Library()
 
+
 @register.simple_tag
-def gravatar(email, size=80):
+def gravatar_url(email, size=80):
     return "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?" + urllib.urlencode({'d':GRAVATAR_DEFAULT_URL, 's':str(size)})
+
+
+@register.simple_tag
+def gravatar(email, size=80, options=""):
+    url = gravatar_url(email, size)
+    return '<img src="%s" width="%s" height="%s" %s />'%(url, size, size, options)
+
