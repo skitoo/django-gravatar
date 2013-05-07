80bb069a60bbcaeb2d865f19542d4fa7d7c1f5bf
Murph Murphy <omurcada@gmail.com>
2013-05-07 11:16:42 -0600

    * Changed spacing over to 4 from my default.

--- django_gravatar/templatetags/gravatar.py
+++ django_gravatar/templatetags/gravatar.py
@@ -1,21 +1,21 @@
 
 from django.template import Library
 from django_gravatar.settings import GRAVATAR_DEFAULT_URL, GRAVATAR_SECURE
-import urllib, hashlib
+import urllib
+import hashlib
 
 register = Library()
 
 
 @register.simple_tag
 def gravatar_url(email, size=80):
-   if GRAVATAR_SECURE == True:
-      return "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?" + urllib.urlencode({'d':GRAVATAR_DEFAULT_URL, 's':str(size)})
-   else:
-    return "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?" + urllib.urlencode({'d':GRAVATAR_DEFAULT_URL, 's':str(size)})
+    if GRAVATAR_SECURE:
+        return "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?" + urllib.urlencode({'d': GRAVATAR_DEFAULT_URL, 's': str(size)})
+    else:
+        return "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?" + urllib.urlencode({'d': GRAVATAR_DEFAULT_URL, 's': str(size)})
 
 
 @register.simple_tag
 def gravatar(email, size=80, options=""):
     url = gravatar_url(email, size)
-    return '<img src="%s" width="%s" height="%s" %s />'%(url, size, size, options)
-
+    return '<img src="%s" width="%s" height="%s" %s />' % (url, size, size, options)
