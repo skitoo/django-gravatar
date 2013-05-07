e1103b06c9ba2e81d54d4528b4f96da20170aec6
Murph Murphy <omurcada@gmail.com>
2013-05-07 11:11:50 -0600

    * Added HTTPS support.

--- django_gravatar/templatetags/gravatar.py
+++ django_gravatar/templatetags/gravatar.py
@@ -1,6 +1,6 @@
 
 from django.template import Library
-from django_gravatar.settings import GRAVATAR_DEFAULT_URL
+from django_gravatar.settings import GRAVATAR_DEFAULT_URL, GRAVATAR_SECURE
 import urllib, hashlib
 
 register = Library()
@@ -8,6 +8,9 @@ register = Library()
 
 @register.simple_tag
 def gravatar_url(email, size=80):
+   if GRAVATAR_SECURE == True:
+      return "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?" + urllib.urlencode({'d':GRAVATAR_DEFAULT_URL, 's':str(size)})
+   else:
     return "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?" + urllib.urlencode({'d':GRAVATAR_DEFAULT_URL, 's':str(size)})
 
 
