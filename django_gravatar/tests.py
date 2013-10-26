7788165fa2bd42e2b92b3f38d052764b69b70397
skitoo <alexis.couronne@gmail.com>
2013-10-26 16:03:41 +0200

    add test for gravatar function

--- django_gravatar/tests.py
+++ django_gravatar/tests.py
@@ -8,3 +8,13 @@ class TestGravatar(unittest.TestCase):
     def test_gravatar_url(self):
         self.assertEquals('http://www.gravatar.com/avatar/d7935ea08d17e261d3b0d12c04759c9d?s=80&d=', gravatar_url('alexis.couronne@gmail.com'))
         self.assertEquals('http://www.gravatar.com/avatar/d7935ea08d17e261d3b0d12c04759c9d?s=120&d=', gravatar_url('alexis.couronne@gmail.com', 120))
+
+    def test_gavatar(self):
+        html = '<img src="http://www.gravatar.com/avatar/d7935ea08d17e261d3b0d12c04759c9d?s=80&d=" width="80" height="80"  >'
+        self.assertEquals(html, gravatar('alexis.couronne@gmail.com'))
+
+        html = '<img src="http://www.gravatar.com/avatar/d7935ea08d17e261d3b0d12c04759c9d?s=130&d=" width="130" height="130"  >'
+        self.assertEquals(html, gravatar('alexis.couronne@gmail.com', 130))
+
+        html = '<img src="http://www.gravatar.com/avatar/d7935ea08d17e261d3b0d12c04759c9d?s=180&d=" width="180" height="180" class="avatar" >'
+        self.assertEquals(html, gravatar('alexis.couronne@gmail.com', 180, 'class="avatar"'))
